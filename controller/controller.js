de1a20111a9a335354d6c31b3f8c2ddd75edde08
Anup Chenthamarakshan <anupcshan@gmail.com>
2014-01-25 00:00:00 -0800

    Skeleton controller.

---
+++ controller/controller.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var app = express();
+
+var API_ROOT = '/api/v1';
+var PORT = 5000;
+
+app.get(API_ROOT + '/list', function (request, response) {
+  response.writeHead(200, {'Content-Type' : 'application/json'});
+  response.end(JSON.stringify({'status': 'TODO: List'}));
+});
+
+app.get(API_ROOT + '/read', function (request, response) {
+  response.writeHead(200, {'Content-Type' : 'application/json'});
+  response.end(JSON.stringify({'status': 'TODO: Read'}));
+});
+
+app.get(API_ROOT + '/add', function (request, response) {
+  response.writeHead(200, {'Content-Type' : 'application/json'});
+  response.end(JSON.stringify({'status': 'TODO: Add'}));
+});
+
+app.get(API_ROOT + '/move', function (request, response) {
+  response.writeHead(200, {'Content-Type' : 'application/json'});
+  response.end(JSON.stringify({'status': 'TODO: Move'}));
+});
+
+app.get(API_ROOT + '/search', function (request, response) {
+  response.writeHead(200, {'Content-Type' : 'application/json'});
+  response.end(JSON.stringify({'status': 'TODO: Search'}));
+});
+
+app.get(API_ROOT + '/delete', function (request, response) {
+  response.writeHead(200, {'Content-Type' : 'application/json'});
+  response.end(JSON.stringify({'status': 'TODO: Delete'}));
+});
+
+app.get(API_ROOT + '/update', function (request, response) {
+  response.writeHead(200, {'Content-Type' : 'application/json'});
+  response.end(JSON.stringify({'status': 'TODO: Update'}));
+});
+
+app.listen(PORT);
