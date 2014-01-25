05691ef13f59f2bae81b74a47457421c4f7b674e
Anup Chenthamarakshan <anupcshan@gmail.com>
2014-01-25 04:16:35 -0800

    Almost got Google login working.

--- controller/controller.js
+++ controller/controller.js
@@ -1,8 +1,72 @@
 var express = require('express');
-var app = express();
+var MongoStore = require('connect-mongo')(express);
+
+var everyauth = require('everyauth');
+var config = require('./config');
+
+var UserModel = config.mongoose.model('User', require('./User'));
 
 var API_ROOT = '/api/v1';
-var PORT = 5000;
+var findOrCreateUserByGoogleData = function(googleMetadata, promise) {
+  console.log(googleMetadata);
+  UserModel.find({'id': googleMetadata.id}, function(err, users) {
+    if(err) throw err;
+    if(users.length > 0) {
+       promise.fulfill(users[0]);
+     } else {
+       var user = new UserModel();
+       user.id = googleMetadata.id;
+       user.given_name = googleMetadata.given_name;
+       user.family_name = googleMetadata.family_name;
+       user.picture = googleMetadata.picture;
+       user.access_token = googleMetadata.id_token;
+       user.save(function(err) {
+         if(err) throw err;
+         promise.fulfill(user);
+       });
+     }
+  });
+};
+
+everyauth.google
+  .entryPath('/auth/google')
+  .callbackPath('/auth/google/callback')
+  .appId(config.google.appId)
+  .appSecret(config.google.appSecret)
+  .scope('https://www.googleapis.com/auth/drive.file https://www.googleapis.com/auth/drive.readonly.metadata https://www.googleapis.com/auth/drive.appdata https://www.googleapis.com/auth/userinfo.profile')
+  .authQueryParam({access_type:'online', approval_prompt:'auto'})
+  .findOrCreateUser(function (session, accessToken, accessTokenExtra, googleUserMetadata) {
+    console.log('Session:', session);
+    console.log('Access Token:', accessToken);
+    console.log('Access Token Extra:', accessTokenExtra);
+    console.log('User Metadata:', googleUserMetadata);
+    googleUserMetadata.id_token = accessTokenExtra.id_token;
+    googleUserMetadata.expires_in = accessTokenExtra.expires_in;
+    console.log("User data returned from Google: ", googleUserMetadata);
+    var promise = this.Promise();
+    findOrCreateUserByGoogleData(googleUserMetadata, promise);
+    return promise;
+  })
+  .myHostname(config.hostName)
+  .redirectPath('/');
+
+everyauth.everymodule.findUserById(function(userId, callback) {
+    UserModel.find({'id': userId}, function(err, users) {
+      if(err) throw err;
+      callback(null, users[0]);
+    });
+  });
+
+var app = express();
+
+app.use(express.bodyParser())
+   .use(express.logger())
+   .use(express.cookieParser('miketesting'))
+   .use(express.session({
+      secret: 'FxT10477A93d54HJx5',
+      store: new MongoStore({mongoose_connection: config.mongoose.connections[0]})
+    }))
+   .use(everyauth.middleware());
 
 app.configure(function(){
   app.use(express.static(__dirname + '/webui'));
@@ -13,6 +77,7 @@ app.configure(function(){
 });
 
 app.get(API_ROOT + '/list', function (request, response) {
+  console.log('User', request.user);
   response.writeHead(200, {'Content-Type' : 'application/json'});
   response.end(JSON.stringify({'status': 'TODO: List'}));
 });
@@ -47,7 +112,7 @@ app.get(API_ROOT + '/update', function (request, response) {
   response.end(JSON.stringify({'status': 'TODO: Update'}));
 });
 
-app.get("/", function handler (req, res) {
+app.get("/", function (req, res) {
   var fs = require('fs');
   fs.readFile(__dirname + '/webui/home.html', function (err, data) {
     if (err) {
@@ -59,4 +124,6 @@ app.get("/", function handler (req, res) {
   });
 });
 
-app.listen(PORT);
+app.listen(config.port, function() {
+  console.log('Listening on port ', config.port);
+});
